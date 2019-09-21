5c0a52b88ebfa06d0edef8318ead02071860da9f
Amr Aboelela <amraboelela@gmail.com>
2019-09-21 11:55:32 -0700

    cleanup

--- say.py
+++ say.py
@@ -1,13 +1,13 @@
 # importing the requests library
 import sys, subprocess
 
-if len(sys.argv) > 2:
+if len(sys.argv) > 1:
     filename = sys.argv[1]
-    language = sys.argv[2]
 else:
     print "please provide the file name and its language"
     exit(-1)
 
+language = filename[len(filename)-6:len(filename)-4]
 switcher = {
         "en": "Alex",
         "tr": "Yelda",
@@ -21,10 +21,13 @@ lines = file.read().splitlines()
 count = 0
 for line in lines:
     if not line.strip():
-        print
+        x = 0
     else:
         count = count + 1
-        print("saying: " + line)
-        subprocess.call(["say", "-v", voice, "-o", "audio/" + filename.replace('.srt', '') + "-" + format(count, '04d') + ".m4a", line])
+        print(str(count) + ".saying: " + line)
+        if language == "en":
+            subprocess.call(["say", "-v", voice, "-o", "audio/" + filename.replace('.srt', '') + "-" + format(count, '04d') + ".m4a", line])
+        else:
+            subprocess.call(["say", "-v", voice, "-r", "130", "-o", "audio/" + filename.replace('.srt', '') + "-" + format(count, '04d') + ".m4a", line])
 file.close()
 
