d43dabc1a944409df036f1993d46b7d63556e38e
Amr Aboelela <amraboelela@gmail.com>
2019-09-23 12:12:18 -0700

    combine

--- combine.py
+++ combine.py
@@ -7,10 +7,15 @@ else:
     print "please provide the directory"
     exit(-1)
 
+outputFile = open(directory + ".txt", "w")
 files = os.listdir(directory)
 files.sort()
 for file in files:
     if ".DS_Store" in file:
         x = 1
     else:
-        print "file '" + directory + "/" + file + "'"
+        outputFile.write("file '" + directory + "/" + file + "'\n")
+
+outputFile.close()
+subprocess.call(["ffmpeg", "-f", "concat", "-fase", "0", "-i", directory + ".txt", "-c", "copy", "output"])
+
