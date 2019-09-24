2a0fb9baf153aede7c3cb5b5059db132ac4530d2
Amr Aboelela <amraboelela@gmail.com>
2019-09-24 03:57:13 -0700

    combine.py

--- combine.py
+++ combine.py
@@ -19,11 +19,11 @@ for file in files:
         #silenceFile = directory + "/silence/" + file.replace(".m4a", "-silence.m4a")
         #subprocess.call(["ffmpeg", "-f", "lavfi", "-t", "1", "-i", "anullsrc=channel_layout=stereo:sample_rate=44100", "-i", directory + "/" + file, "-filter_complex", """[1:a][0:a]concat=n=2:v=0:a=1""", "-strict", "-2", silenceFile])
         outputFile.write("file '" + directory + "/" + file + "'\n")
-        outputFile.write("file 'silence.m4a'\n")
+        outputFile.write("file 'silence1.m4a'\n")
         if "-en" not in file:
             outputFile.write("file '" + directory + "/" + file + "'\n")
-            outputFile.write("file 'silence.m4a'\n")
-            outputFile.write("file 'silence.m4a'\n")
+            outputFile.write("file 'silence1.m4a'\n")
+            outputFile.write("file 'silence1.m4a'\n")
 
 outputFile.close()
 subprocess.call(["ffmpeg", "-f", "concat", "-safe", "0", "-i", "audioFiles.txt", "-c", "copy", directory + ".m4a"])
