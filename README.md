f24372e58bdad02096cac62f6b109b71bf5a809e
Amr Aboelela <amraboelela@gmail.com>
2019-09-25 15:07:04 -0700

    combine.py

--- README.md
+++ README.md
@@ -3,11 +3,39 @@ Learn Turkish and other languages through Ertugrul TV series
 
 ## Usage
 
-```
-$ python clean ertugrul-1-01-en.vtt > ertugrul-1-01-en.txt
-$ python translate ertugrul-1-01-en.txt tr > ertugrul-1-01-tr.txt
-$ python say ertugrul-1-01-en.txt
-$ python say ertugrul-1-01-tr.txt
-$ python combine.py ertugrul-1-01 > ertugrul-1-01-audio.txt
+- Do this part in MacOS:
 
 ```
+$ python clean ertugrul-1-01-en.vtt > ertugrul-1-01-en.txt
+$ python translate ertugrul-1-01-en.txt tr 
+$ python say ertugrul-1-01-en.txt
+$ python say ertugrul-1-01-tr.txt
+$ zip -r ertugrul-1-01.zip ertugrul-1-01
+$ scp ertugrul-1-01.zip amr@amrpro.loca:ertugrul
+```
+
+- Then do this part in Linux (Ubuntu)
+
+```
+$ unzip ertugrul-1-01.zip
+$ python combine.py ertugrul-1-01
+```
+
+- Then test the audio file in MacOS:
+
+```
+$ scp amr@amrpro.local:ertugrul/ertugrul-1-01-en.m4a .
+```
+
+- Then generate the video file in Linux:
+
+```
+$ ./video ertugrul-1-01
+```
+
+- Then test the video file in MacOS:
+
+```
+$ scp amr@amrpro.local:ertugrul/ertugrul-1-01-en.mp4 .
+```
+
--- combine.py
+++ combine.py
@@ -10,21 +10,16 @@ else:
 outputFile = open("audioFiles.txt", "w")
 files = os.listdir(directory)
 files.sort()
-#print directory
-#subprocess.call(["mkdir", directory + "/silence"])
 for file in files:
     if ".DS_Store" in file:
         x = 1
     else:
-        #silenceFile = directory + "/silence/" + file.replace(".m4a", "-silence.m4a")
-        #subprocess.call(["ffmpeg", "-f", "lavfi", "-t", "1", "-i", "anullsrc=channel_layout=stereo:sample_rate=44100", "-i", directory + "/" + file, "-filter_complex", """[1:a][0:a]concat=n=2:v=0:a=1""", "-strict", "-2", silenceFile])
         outputFile.write("file '" + directory + "/" + file + "'\n")
         outputFile.write("file 'silence1.m4a'\n")
         if "-en" not in file:
-            #outputFile.write("file '" + directory + "/" + file + "'\n")
-            #outputFile.write("file 'silence.m4a'\n")
             outputFile.write("file 'silence1.m4a'\n")
 
 outputFile.close()
 subprocess.call(["ffmpeg", "-f", "concat", "-safe", "0", "-i", "audioFiles.txt", "-c", "copy", directory + ".m4a"])
+#subprocess.call(["ffmpeg", "-loop", "1", "-i", directory + ".png", "-i", directory + ".m4a", "-c:v", "libx264", "-tune", "stillimage", "-c:a", "aac", "-b:a", "192k", "-pix_fmt", "yuv420p", "-shortest", directory + ".mp4"])
 
