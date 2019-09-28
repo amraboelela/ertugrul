77d535cec3dacc385b008fe7aa37105b61b2a02d
Amr Aboelela <amraboelela@gmail.com>
2019-09-28 06:58:53 -0700

    ertugrul-1-06-en.txt

--- clean.py
+++ clean.py
@@ -12,15 +12,32 @@ else:
     print "please provide the file name"
     exit(-1)
 
+cleanArray = ["-->", "color=", "9999"]
+cleanArray2 = ["WEBVTT", "Kind: captions", "Language:", "RESURRECTION ERTUGRUL", "THE STORIES AND CHARACTERS DEPICTED", "HERE WERE INSPIRED BY OUR HISTORY", "NO ANIMALS WERE HARMED DURING", "THE FILMING OF THIS PRODUCTION", "Translation:"]
+
 file = open(filename) 
 lines = file.read().splitlines()
 for line in lines:
-    if not line.strip():
-        print
-    elif "-->" in line or len(line) < 4 or "color=" in line or "9999" in line:
-        x = 0
-    else:
-        line = remove_tags(line)
-        line = line.replace('-','')
-        print line
+    skipLine = False
+    if len(line) < 4:
+        skipLine = True
+    for token in cleanArray:
+        if token in line:
+            skipLine = True
+            break
+    if "ertugrul-1-01" not in filename:
+        for token in cleanArray2:
+            if token in line:
+                skipLine = True
+                break
+    if not skipLine:
+        if not line.strip():
+            print
+        elif "-->" in line or len(line) < 4 or "color=" in line or "9999" in line:
+            x = 0
+        else:
+            line = remove_tags(line)
+            line = line.replace('-','')
+            print line
 file.close()
+
